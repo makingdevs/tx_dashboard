0a6477e63244561013b1e8d9937d9ef97eff8a27
Jose Juan Reyes <neodevelop@gmail.com>
2022-03-03 09:01:17 -0600

    Add page to LiveView, handle click event

--- lib/tx_dashboard_web/live/balances/account_status_live.ex
+++ lib/tx_dashboard_web/live/balances/account_status_live.ex
@@ -9,15 +9,20 @@ defmodule TxDashboardWeb.Balances.AccountStatusLive do
   @impl true
   def mount(%{"account_number" => account_number} = _params, _session, socket) do
     Phoenix.PubSub.subscribe(TxDashboard.PubSub, @topic <> ":" <> account_number)
-    transactions = Dashboard.list_transactions()
-    {:ok, assign(socket, account_number: account_number, transactions: transactions)}
+
+    %{entries: transactions} = page = Dashboard.list_transactions_by_account(account_number)
+
+    {:ok, assign(socket, account_number: account_number, transactions: transactions, page: page)}
   end
 
   # def handle_params() do
   # end
 
-  # def handle_event() do
-  # end
+  @impl true
+  def handle_event("page_number", %{"page-number" => page_number}, socket) do
+    IO.inspect(binding())
+    {:noreply, socket}
+  end
 
   @impl true
   def handle_info(%Transaction{} = transaction, socket) do
