5e40c64bf268c23356d7a137bbc7f4b6f7b528f0
Jose Juan Reyes <neodevelop@gmail.com>
2022-03-03 08:59:02 -0600

    Add Scrivener dep

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule TxDashboard.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:amqp, "~> 3.1"}
+      {:amqp, "~> 3.1"},
+      {:scrivener_ecto, "~> 2.0"}
     ]
   end
 
