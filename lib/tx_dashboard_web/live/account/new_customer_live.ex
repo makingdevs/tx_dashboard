6678bcd2ad2b7f2be0c63eb83d9ca86259165d50
Jose Juan Reyes <neodevelop@gmail.com>
2022-03-04 08:43:49 -0600

    Add New Account Live View

--- lib/tx_dashboard_web/live/account/new_customer_live.ex
+++ lib/tx_dashboard_web/live/account/new_customer_live.ex
@@ -1,3 +1,52 @@
 defmodule TxDashboardWeb.Account.NewCustomerLive do
   use TxDashboardWeb, :live_view
+
+  alias TxDashboard.Dashboard
+  alias TxDashboard.Dashboard.Account
+
+  @impl true
+  def mount(_params, _session, socket) do
+    changeset = Dashboard.change_account(%Account{}, %{})
+    {:ok, assign(socket, changeset: changeset, account: %{})}
+  end
+
+  @impl true
+  def handle_event("validate", %{"account" => account}, socket) do
+    changeset =
+      Dashboard.change_account(%Account{}, account)
+      |> Map.put(:action, :insert)
+      |> IO.inspect()
+
+    {:noreply, assign(socket, changeset: changeset, account: account)}
+  end
+
+  @impl true
+  def handle_event("save", %{"account" => _account}, socket) do
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("generic_click", _params, socket) do
+    send(self(), {"new_event", %{"account" => %{"name" => "Mr Process"}}})
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info({"new_event", %{"account" => account}}, state) do
+    changeset =
+      Dashboard.change_account(%Account{}, account)
+      |> Map.put(:action, :insert)
+
+    {:noreply, assign(state, changeset: changeset, account: account)}
+  end
+
+  defp summary(%{"name" => name, "lastname" => lastname, "account" => account}) do
+    name
+    |> Kernel.<>(" ")
+    |> Kernel.<>(lastname)
+    |> Kernel.<>(" ")
+    |> Kernel.<>(account)
+  end
+
+  defp summary(_), do: ""
 end
