8b63f0d542aec6fa6a6c8132e9f19dcd67495893
Jose Juan Reyes <neodevelop@gmail.com>
2022-03-04 20:55:50 -0600

    Add Credo and Dyalizer

--- mix.exs
+++ mix.exs
@@ -50,7 +50,9 @@ defmodule TxDashboard.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:amqp, "~> 3.1"},
-      {:scrivener_ecto, "~> 2.0"}
+      {:scrivener_ecto, "~> 2.0"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
