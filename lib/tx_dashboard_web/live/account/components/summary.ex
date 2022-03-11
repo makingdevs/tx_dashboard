8c19b58144a9d786682f6533bead21ac0c3dca9e
Jose Juan Reyes <neodevelop@gmail.com>
2022-03-11 08:18:25 -0600

    Add a live component

---
+++ lib/tx_dashboard_web/live/account/components/summary.ex
@@ -0,0 +1,32 @@
+defmodule TxDashboardWeb.Account.Components.Summary do
+  use TxDashboardWeb, :live_component
+
+  @impl true
+  def mount(socket) do
+    socket =
+      socket
+      |> assign(name: "", lastname: "", account: "")
+
+    {:ok, socket}
+  end
+
+  @impl true
+  def update(
+        %{account: %{"name" => name, "lastname" => lastname, "account" => account}} = _assigns,
+        socket
+      ) do
+    socket =
+      socket
+      |> assign(
+        name: String.capitalize(name),
+        lastname: String.capitalize(lastname),
+        account: account
+      )
+
+    {:ok, socket}
+  end
+
+  def update(_assigns, socket) do
+    {:ok, socket}
+  end
+end
