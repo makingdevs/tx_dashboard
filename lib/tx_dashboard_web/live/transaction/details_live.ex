5f8ba4ec91cb786b0bcf1ce08c139e8ab07dc6f1
Jose Juan Reyes <neodevelop@gmail.com>
2022-03-12 09:08:33 -0600

    Uploading Files with Phx Live View

--- lib/tx_dashboard_web/live/transaction/details_live.ex
+++ lib/tx_dashboard_web/live/transaction/details_live.ex
@@ -5,6 +5,11 @@ defmodule TxDashboardWeb.Transaction.DetailsLive do
 
   @impl true
   def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(:uploaded_files, [])
+      |> allow_upload(:document, accept: ~w/.jpg .png/, max_entries: 3)
+
     {:ok, socket}
   end
 
@@ -18,6 +23,33 @@ defmodule TxDashboardWeb.Transaction.DetailsLive do
     {:noreply, socket}
   end
 
+  @impl true
+  def handle_event("validate", _params, socket) do
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("cancel-upload", %{"ref" => ref}, socket) do
+    socket = cancel_upload(socket, :document, ref)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("save", _params, socket) do
+    uploaded_files =
+      consume_uploaded_entries(socket, :document, fn %{path: path}, _entry ->
+        {:ok, path}
+      end)
+      |> IO.inspect(label: "FILES")
+
+    socket = update(socket, :uploaded_files, &(&1 ++ uploaded_files))
+    {:noreply, socket}
+  end
+
   defp page_title(:show), do: "Show Transaction"
   defp page_title(:edit), do: "Edit Transaction"
+
+  def error_to_string(:too_large), do: "Too large"
+  def error_to_string(:not_accepted), do: "You have selected an unacceptable file type"
+  def error_to_string(:too_many_files), do: "You have selected too many files"
 end
